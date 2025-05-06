dc27eaa6e3756120575a13a80cec3c3575c5ec7f
sylviezhang37 <114536986+sylviezhang37@users.noreply.github.com>
2025-05-06 22:15:17 +0900

    updated repo

---
+++ backend/reviews/repo.ts
@@ -0,0 +1,45 @@
+import { Pool } from "pg";
+import { Review } from "./domain";
+
+interface ReviewsRepo {
+  findByPlaceId(placeId: string): Promise<Review[]>;
+  create(review: Review): Promise<Review>;
+}
+
+export class PostgresReviewsRepo implements ReviewsRepo {
+  constructor(private readonly dbConnection: Pool) {}
+
+  async create(review: Review): Promise<Review> {
+    const query = `
+      INSERT INTO reviews (place_id, user_id, pet_friendly, comment, created_at)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING *
+    `;
+
+    const result = await this.dbConnection.query(query, [
+      review.placeId,
+      review.userId,
+      review.petFriendly,
+      review.comment,
+      review.createdAt,
+    ]);
+
+    return this.mapToEntity(result.rows[0]);
+  }
+
+  async findByPlaceId(placeId: string): Promise<Review[]> {
+    // TODO: Implement
+    return [];
+  }
+
+  async mapToEntity(data: any): Promise<Review> {
+    return new Review({
+      id: data.id,
+      placeId: data.place_id,
+      userId: data.user_id,
+      petFriendly: data.pet_friendly,
+      comment: data.comment,
+      createdAt: data.created_at,
+    });
+  }
+}
