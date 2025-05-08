c87493c0360b134665e45e4f739ed83d052f839b
sylviezhang37 <114536986+sylviezhang37@users.noreply.github.com>
2025-05-08 12:49:03 +0900

    add user repo

---
+++ backend/users/src/repo.ts
@@ -0,0 +1,43 @@
+import { Pool } from "pg";
+import { User } from "./domain";
+
+interface UsersRepo {
+  create(user: User): Promise<User>;
+  getById(id: string): Promise<User | null>;
+}
+
+export class PostgresUsersRepo implements UsersRepo {
+  constructor(private readonly dbConnection: Pool) {}
+
+  async create(user: User): Promise<User> {
+    const query = `
+        INSERT INTO users (id, username, created_at, anonymous)
+        VALUES ($1, $2, $3, $4)
+        RETURNING *
+    `;
+
+    const result = await this.dbConnection.query(query, [
+      user.id,
+      user.username,
+      user.createdAt,
+      user.anonymous,
+    ]);
+
+    return this.mapToDomain(result.rows[0]);
+  }
+
+  async getById(id: string): Promise<User | null> {
+    const query = `SELECT * FROM users where id = $1`;
+    const result = await this.dbConnection.query(query, [id]);
+    return result.rows.length ? this.mapToDomain(result.rows[0]) : null;
+  }
+
+  private mapToDomain(data: any): User {
+    return new User({
+      id: data.id,
+      username: data.usename,
+      createdAt: data.created_at,
+      anonymous: data.anonymous,
+    });
+  }
+}
